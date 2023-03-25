c5c34fe04e32831873cc85f32d1249f10bebdae6
DANDU HARI PRASAD REDDY <113785719+HARIPRASADREDDY-DANDU@users.noreply.github.com>
2023-03-25 20:13:33 +0530

    Add files via upload

---
+++ BMI.js
@@ -0,0 +1,14 @@
+// alert("Hello World!")
+
+// const age = 20;
+// const req = 18;
+// console.log(age > req, age < req);
+
+const marksMass = 78, marksHeight = 1.69;
+const johnMass = 92, johnHeight = 1.95;
+marksBmi = marksMass/marksHeight**2;
+console.log(marksBmi);
+johnBmi = johnMass/johnHeight**2;
+console.log(johnBmi)
+highestBmi = marksBmi > johnBmi;
+console.log(highestBmi);
